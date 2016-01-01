33c7102215cb827873ba25360f5663e6c839c34c
alexpfx <alexpfba@gmail.com>
2016-01-01 20:55:05 -0200

    criacao de listener para mercado ribeirao.

---
+++ src/main/java/br/com/alexpfx/supermarket/crawler/jaunt/MercadoRibeiraoProductExtractListener.java
@@ -0,0 +1,18 @@
+package br.com.alexpfx.supermarket.crawler.jaunt;
+
+import br.com.alexpfx.supermarket.crawler.model.bo.ProductBo;
+import br.com.alexpfx.supermarket.crawler.model.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by alexandre on 01/01/2016.
+ */
+public class MercadoRibeiraoProductExtractListener implements ProductExtractedListener {
+
+    @Autowired
+    private ProductBo productBo;
+    @Override
+    public void productExtracted(Product product) {
+        productBo.save(product);
+    }
+}
