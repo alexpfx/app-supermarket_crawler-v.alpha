b5b10583c4b6346bc5a668198c929cf4a9272256
alexpfx <alexpfba@gmail.com>
2016-01-13 21:48:48 -0200

    separando configuracao de job de beans.

---
+++ src/main/java/br/com/alexpfx/supermarket/batch/configuration/Beans.java
@@ -0,0 +1,52 @@
+package br.com.alexpfx.supermarket.batch.configuration;
+
+import br.com.alexpfx.supermarket.batch.reader.ProductList;
+import br.com.alexpfx.supermarket.bo.ProductBo;
+import br.com.alexpfx.supermarket.bo.impl.ProductBoImpl;
+import br.com.alexpfx.supermarket.dao.ProductDao;
+import br.com.alexpfx.supermarket.dao.impl.ProductDaoImpl;
+import br.com.alexpfx.supermarket.webcrawler.crawler.Crawler;
+import br.com.alexpfx.supermarket.webcrawler.crawler.impl.RibeiraoCrawler;
+import br.com.alexpfx.supermarket.webcrawler.factory.UserAgentFactory;
+import br.com.alexpfx.supermarket.webcrawler.listeners.CrawlerListener;
+import br.com.alexpfx.supermarket.webcrawler.listeners.impl.RibeiraoListener;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by alexandre on 13/01/2016.
+ */
+@Configuration
+@EnableBatchProcessing
+public class Beans {
+
+    @Bean
+    public Crawler crawler() {
+        return new RibeiraoCrawler(new UserAgentFactory());
+    }
+
+    @Bean
+    public ProductBo productBo() {
+        return new ProductBoImpl();
+    }
+
+    @Bean
+    public ProductDao productDao() {
+        return new ProductDaoImpl();
+    }
+
+
+    @Bean
+    public CrawlerListener listener() {
+        CrawlerListener listener = new RibeiraoListener();
+        return listener;
+    }
+
+    @Bean
+    public ProductList getProductList() {
+        return new ProductList();
+    }
+
+
+}
--- src/main/java/br/com/alexpfx/supermarket/batch/configuration/CrawlerJobConfiguration.java
+++ src/main/java/br/com/alexpfx/supermarket/batch/configuration/CrawlerJobConfiguration.java
@@ -2,19 +2,9 @@ package br.com.alexpfx.supermarket.batch.configuration;
 
 import br.com.alexpfx.supermarket.batch.processor.ProductProcessor;
 import br.com.alexpfx.supermarket.batch.reader.ProductItemReader;
-import br.com.alexpfx.supermarket.batch.reader.ProductList;
 import br.com.alexpfx.supermarket.batch.tasklet.StartCrawlerTasklet;
 import br.com.alexpfx.supermarket.batch.writer.HIbernateProductsItemWriter;
-import br.com.alexpfx.supermarket.bo.ProductBo;
-import br.com.alexpfx.supermarket.bo.impl.ProductBoImpl;
-import br.com.alexpfx.supermarket.dao.ProductDao;
-import br.com.alexpfx.supermarket.dao.impl.ProductDaoImpl;
 import br.com.alexpfx.supermarket.domain.Product;
-import br.com.alexpfx.supermarket.webcrawler.crawler.Crawler;
-import br.com.alexpfx.supermarket.webcrawler.crawler.impl.RibeiraoCrawler;
-import br.com.alexpfx.supermarket.webcrawler.factory.UserAgentFactory;
-import br.com.alexpfx.supermarket.webcrawler.listeners.CrawlerListener;
-import br.com.alexpfx.supermarket.webcrawler.listeners.impl.RibeiraoListener;
 import br.com.alexpfx.supermarket.webcrawler.to.TransferObject;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -22,8 +12,6 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.AbstractJob;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
@@ -54,28 +42,17 @@ public class CrawlerJobConfiguration {
 
     @Bean
     public Job job(JobBuilderFactory jobs) {
-        Job theJob = jobs.get("theJob").start(flow()).end().build();
+        Job theJob = jobs.get("job").start(crawlerStep()).next(processProductStep()).build();
         ((AbstractJob) theJob).setRestartable(true);
         return theJob;
 
     }
 
-
-    protected Step crawlerStep() {
+    private Step crawlerStep() {
         return steps.get("crawlerStep").tasklet(crawlerTasklet()).build();
     }
 
-    private Flow flow() {
-        FlowBuilder<Flow> builder = new FlowBuilder<>("flow1");
-        builder.start(crawlerStep())
-                .next(processProductStep())
-                .end();
-        return builder.build();
-    }
-
-
-    @Bean
-    protected Step processProductStep() {
+    private Step processProductStep() {
         return steps.get("processProductStep")
                 .<TransferObject, Product>chunk(10)
                 .reader(reader())
@@ -84,53 +61,20 @@ public class CrawlerJobConfiguration {
                 .build();
     }
 
-    @Bean
-    protected Tasklet crawlerTasklet() {
+    private Tasklet crawlerTasklet() {
         return new StartCrawlerTasklet();
     }
 
-
-    @Bean
-    protected ProductList getProductList() {
-        return new ProductList();
-    }
-
-    @Bean
-    public CrawlerListener listener() {
-        String dbUrl = environment.getProperty("db.url");
-
-        CrawlerListener listener = new RibeiraoListener();
-        return listener;
-    }
-
-    @Bean
-    public ItemProcessor<TransferObject, Product> processor() {
+    private ItemProcessor<TransferObject, Product> processor() {
         return new ProductProcessor();
     }
 
-    @Bean
-    public ItemReader<TransferObject> reader() {
+    private ItemReader<TransferObject> reader() {
         return new ProductItemReader();
     }
 
-    @Bean
-    public ItemWriter<Product> writer() {
+    private ItemWriter<Product> writer() {
         return new HIbernateProductsItemWriter();
-    }
-
-    @Bean
-    public Crawler crawler() {
-        return new RibeiraoCrawler(new UserAgentFactory());
-    }
-
-    @Bean
-    public ProductBo productBo() {
-        return new ProductBoImpl();
-    }
-
-    @Bean
-    public ProductDao productDao() {
-        return new ProductDaoImpl();
     }
 
 
