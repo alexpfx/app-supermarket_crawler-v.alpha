d8ad0178fa4a912030e21531ed18729f4f176a85
alexpfx <alexpfba@gmail.com>
2016-01-30 21:21:42 -0200

    droga.

--- src/main/java/br/com/alexpfx/supermarket/webcrawler/crawler/api/visitor/VisitorApi.java
+++
@@ -1,8 +0,0 @@
-package br.com.alexpfx.supermarket.webcrawler.crawler.api.visitor;
-
-/**
- * Created by alexandre on 30/01/2016.
- */
-public interface VisitorApi {
-    String visit (String url);
-}
